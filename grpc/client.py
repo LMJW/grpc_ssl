b8c780cde370767ef57f7e88ff45b6a6ca1bf537
LMJW <heysuperming@gmail.com>
2018-01-02 16:03:10 +1100

    Build a simple grpc client server module. Working properly on the localhost.

---
+++ grpc/client.py
@@ -0,0 +1,22 @@
+"""
+Description: This file is a test example of realizing the grpc communication between
+             aws cloud and a local computer.
+"""
+import grpc
+
+import bfawstest_pb2
+import bfawstest_pb2_grpc
+
+_REQ_MSG = dict(testtextmessage=input("Input the request message:\n"))
+
+def run():
+    """
+    Define the run program.
+    """
+    channel = grpc.insecure_channel('localhost:22222')
+    stub = bfawstest_pb2_grpc.TestAWSServiceStub(channel)
+    response = stub.Getdatabase(bfawstest_pb2.awsrequest(**_REQ_MSG))
+    print(response)
+
+if __name__ == "__main__":
+    run()
---
+++ grpc/server.py
@@ -0,0 +1,72 @@
+from concurrent import futures
+import base64
+import time 
+import sqlite3
+
+import bfawstest_pb2
+import bfawstest_pb2_grpc
+
+import grpc
+
+def request_to_sql(db_file, request):
+    """
+    This function stores the request into a sqlite3 database
+    """
+    db_cursor = db_file.cursor()
+    try:
+        db_cursor.execute('''CREATE table IF NOT EXISTS request (Time, info)''')
+        db_cursor.execute('INSERT INTO request VALUES ("%s", "%s")'%\
+            (time.ctime(), request.testtextmessage))
+        # get the query results from database
+        try:
+            db_cursor.execute('SELECT _rowid_, * FROM request')
+            _datalist = []
+            while True:
+                _numorder, _time, _info = db_cursor.fetchone()
+                print(_numorder, _time, _info)
+                _datalist.append(bfawstest_pb2.COUNTERANDTIMER(\
+                        orderofrequest=_numorder, requesttime=_time, requestinfo=_info))
+        except TypeError as err:
+            print(err)
+            _response = bfawstest_pb2.awsresponse(responsemsg="Query successfully",\
+                    countertimer=_datalist)
+            return _response
+    except sqlite3.OperationalError as e:
+        print(e)
+        return bfawstest_pb2.awsresponse(responsemsg="Sever database query error")
+
+class AWSQueryService(bfawstest_pb2_grpc.TestAWSServiceServicer):
+    """
+    This class defines the simple aws query service
+    """
+    def __init__(self):
+        """
+        Attach the database to the class
+        """
+        # self.db = sqlite3.connect('test.db')
+        # Direct connect to database does not support concurrency,
+        # so I change this into a memory format.
+        self.db = sqlite3.connect(":memory:", check_same_thread=False)
+
+    def Getdatabase(self, request, context):
+        """
+        The service source code
+        """
+        return request_to_sql(self.db, request)
+
+def serve():
+    """
+    The server program.
+    """
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    bfawstest_pb2_grpc.add_TestAWSServiceServicer_to_server(AWSQueryService(),server)
+    server.add_insecure_port('[::]:22222')
+    server.start()
+    try:
+        while True:
+            time.sleep(60*60*24)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
